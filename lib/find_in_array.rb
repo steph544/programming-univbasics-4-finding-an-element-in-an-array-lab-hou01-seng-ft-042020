51504a2642495462d7ba19774888a97540c0d080
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-08 15:59:56 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,9 +2,7 @@ def find_element_index(array, value_to_find)
   counter=0 
   while counter<array.length do 
     if array[counter] == value_to_find 
-      puts array.index(value_to_find) 
-    else
-      puts nil 
+     return counter 
     end 
     counter+=1 
   end 
